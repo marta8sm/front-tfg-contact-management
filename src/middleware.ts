09e2748401a0d7a4d3a8123dd6b8b479667922aa
marta8sm <martasanchezm98@usal.es>
2024-07-26 22:12:34 +0200

    Middleware added and .env changed

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { authOptions } from '@/auth/providers/auth/auth.options'
+import { getServerSession } from 'next-auth'
+import { withAuth } from 'next-auth/middleware'
+
+export default withAuth({
+    pages: authOptions.pages,
+    callbacks: {
+        authorized({ req, token }) {
+            if (token) return true
+            const pathname = req.nextUrl.pathname
+
+            return (
+                /*pathname.startsWith('/_next/') ||
+                pathname.startsWith('/favicon.ico') ||
+                pathname.startsWith('/assets/')*/
+                pathname.startsWith('/home')
+            )
+        },
+    },
+})
